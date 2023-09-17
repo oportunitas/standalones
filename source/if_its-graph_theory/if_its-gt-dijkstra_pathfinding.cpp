e1c1184c2bfeaa53d2b0436e0251dd286e3d4fe7
Oportunitas <123826702+Oportunitas@users.noreply.github.com>
2023-09-18 02:24:38 +0700

    update

--- source/if_its-graph_theory/if_its-gt-dijkstra_pathfinding.cpp
+++ source/if_its-graph_theory/if_its-gt-dijkstra_pathfinding.cpp
@@ -1,4 +1,4 @@
-/**
+﻿/**
  * IF ITS Graph Theory
  * Shortest Paths
  * Dijkstra Algorithm
@@ -28,7 +28,7 @@ using namespace std;
 
 class Frontier {
 public:
-    lli dist = 0;
+    lli dist = LLONG_MAX;
     lli vertex = -1;
     sp<Frontier> via = nullptr;
 };
@@ -54,54 +54,112 @@ bool sortVertexThenWeight(
 void dijkstraFind(vec<pair<pair<lli, lli>, lli>> graph, lli from, lli to) {
     dq<Frontier> frontiers;
     frontiers.push_back({ 0, from, nullptr });
-    vec<bool> visited(10000007, false);
-    while (true) {
-        for (auto it : frontiers) {
-            cout
-                << it.dist << tab
-                << it.vertex << endl;
-        } cout << endl;
+    vec<bool> visited(1000006, false);
 
+    while (frontiers.size() > 0) {
+        cout
+            << "<---------------------------------" 
+            << "--------------------------------->"
+            << endl
+            << "Exhausting vertex {" << frontiers.front().vertex << "}"
+            << endl << endl;
         if (frontiers.front().vertex == to) {
+            cout
+                << "But wait! vertex {" << frontiers.front().vertex << "}"
+                << " is the destination!" << endl;
+
+            dq<Frontier> shortest_path;
+            Frontier temp = frontiers.front();
+            shortest_path.push_front(temp);
+            while (temp.via != nullptr) {
+                temp = *temp.via;
+                shortest_path.push_front(temp);
+            }
+
+            cout << "Shortest path found! the path is as follows:" << endl;
+            for (lli k = 0; k < shortest_path.size() - 1; ++k) {
+                cout
+                    << tab << k + 1
+                    << ". Go from vertex {" << shortest_path[k].vertex
+                    << "} to vertex {" << shortest_path[k + 1].vertex
+                    << "}," << endl << tab << "   this will take "
+                    << shortest_path[k + 1].dist - shortest_path[k].dist
+                    << " unit(s) of distance." << endl << endl;
+            }
+            cout
+                << "And there you have it!" << endl
+                << "that is the shortest path between "
+                << "vertex {" << shortest_path.front().vertex << "} and vertex {"
+                << shortest_path.back().vertex << "}," << endl
+                << "which took only " << shortest_path.back().dist
+                << " units of distance" << endl << endl << endl;
             break;
         }
-
-        auto front_pos = find_if(
-            graph.begin(), graph.end(),
+        auto edges_pos = find_if(
+            graph.begin(),
+            graph.end(),
             [&frontiers](pair<pair<lli, lli>, lli>& element) {
                 return element.first.first == frontiers.front().vertex;
             }
         );
 
-        while (front_pos->first.first == frontiers.front().vertex) { 
-            auto pos = find_if(
-                frontiers.begin(),
-                frontiers.end(),
-                [&front_pos](Frontier& element) {
-                    return element.vertex == front_pos->first.second;
-                }
-            );
+        while (edges_pos->first.first == frontiers.front().vertex) {
+            cout
+                << "Checking connection between vertex {"
+                << edges_pos->first.first << "} and vertex {"
+                << edges_pos->first.second << "}" << endl;
 
-            if (pos != frontiers.end()) {
-                if (pos->dist > (frontiers.front().dist + front_pos->second)) {
-                    pos->dist = (frontiers.front().dist + front_pos->second);
+            if (!visited[edges_pos->first.second]) {
+                auto pos = find_if(
+                    frontiers.begin(),
+                    frontiers.end(),
+                    [edges_pos](Frontier& element) {
+                        return element.vertex == edges_pos->first.second;
+                    }
+                );
+                if (pos == frontiers.end()) {
+                    frontiers.push_back({
+                        edges_pos->second + frontiers.front().dist,
+                        edges_pos->first.second,
+                        ms<Frontier>(frontiers.front())
+                        }
+                    );
+
+                    cout
+                        << tab << "established new connection between vertex {"
+                        << edges_pos->first.first << "} and vertex {"
+                        << edges_pos->first.second << "}" << endl << endl;
+                }
+                else if (edges_pos->second + frontiers.front().dist < pos->dist) {
+                    pos->dist = edges_pos->second + frontiers.front().dist;
                     pos->via = ms<Frontier>(frontiers.front());
+
+                    cout
+                        << tab << "changed vertex {" << edges_pos->first.second
+                        << "} to now connect to vertex {"
+                        << edges_pos->first.first << endl << endl;
                 }
             } else {
-                frontiers.push_back({
-                    frontiers.front().dist + front_pos->second,
-                    front_pos->first.second,
-                    ms<Frontier>(frontiers.front())
-                });
-                visited[front_pos->first.second] = true;
-                goto replay;
+                cout
+                    << tab 
+                    << "The destination vertex has already been exhausted."
+                    << endl << tab 
+                    << "continuing to next edge..." << endl << endl;
             }
-            front_pos++;
-        }
+            edges_pos++;
+        } 
+        visited[frontiers.front().vertex] = true;
         frontiers.pop_front();
 
-    replay:
         sort(frontiers.begin(), frontiers.end(), sortFrontierByDist);
+        cout << "-----------------" << endl;
+        cout << "| front | dist  |" << endl;
+        for (auto it : frontiers) {
+            cout 
+                << "|  " << it.vertex << tab 
+                << "|  " << it.dist << tab 
+                << "|  " << endl;
+        } cout << "-----------------" << endl << endl;
     }
 }
 
@@ -125,13 +183,46 @@ int main() {
     }
 
     sort(graph->begin(), graph->end(), sortVertexThenWeight);
-    for (auto it : *graph) {
-        cout
-            << it.first.first << tab
-            << it.first.second << tab << tab
-            << it.second << endl;
-    }
     dijkstraFind(*graph, 1, 13);
+
+    cout
+        << "<---------------------------------"
+        << "--------------------------------->"
+        << endl
+        << "This program is built solely by Oportunitas (Taib Izzat Samawi)"
+        << endl << "for the purpose of completing a graph theory assignment"
+        << endl << "given in ITS Surabaya,"
+        << endl << "namely, finding the shortest path between 2 vertices "
+        << "in a graph." << endl
+        << "<---------------------------------"
+        << "--------------------------------->"
+        << endl;
 
     return 0;
 }
+
+/*
+* input:
+    1 2 1
+    1 3 3
+    1 4 5
+    2 7 7
+    2 5 4
+    3 5 1
+    4 6 2
+    5 8 6
+    5 6 2
+    6 7 1
+    6 8 4
+    6 12 1
+    7 9 6
+    8 10 1
+    9 10 2
+    9 13 4
+    10 11 1
+    10 13 7
+    11 12 4
+    12 13 3
+    0 0 0
+*/
+
